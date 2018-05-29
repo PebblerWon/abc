7505cc0114af5c5658e19f459313322e1f2d8d5e
pebblerwon <pebblerwon@gmail.com>
2018-05-29 19:02:31 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-import 'bootstrap-treeview/dist/bootstrap-treeview.min.css'
-import 'bootstrap-treeview/dist/bootstrap-treeview.min.js'
-
