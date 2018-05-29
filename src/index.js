1129294df2d68942e597294ec85639f6329f728e
pebblerwon <pebblerwon@gmail.com>
2018-05-29 18:26:20 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap-treeview/dist/bootstrap-treeview.min.css'
+import 'bootstrap-treeview/dist/bootstrap-treeview.min.js'
+
